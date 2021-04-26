d7d367b1bbcad13b1d4f0825099efd94381ef295
FelipePTO <felipepaulotavares@gmail.com>
2021-04-25 23:48:33 -0300

    Creating first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [HistoryList, setHistoryList] = useState([])
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState({dish:"", input:""})
+  const [Error, setError] = useState("")
+
+  const AddOrder = async () =>{
+      let data = await fetch("https://localhost:5001/Restaurant", {
+        method: "POST",
+        body: JSON.stringify({
+          listDishes: input
+        }),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      });
+      
+      if(data.status===200){
+        let json = await data.json();
+        setOutput(json)
+        setTimeout(()=>{
+          setOutput({dish:"", input:""})
+        },5000)
+        await getHisoryList();
+      }
+
+      if(data.status===500){
+        setError(await data.json())
+      }
+    
+  }
+
+  const getHisoryList = async () => {
+    let data = await fetch("https://localhost:5001/Restaurant");
+    let json = await data.json();
+    setHistoryList(json)
+  }
+
+  useEffect(() => {
+    getHisoryList();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="formOrder">
+        <div>
+          <input type="input" onChange={(e)=>setInput(e.target.value)} value={input}></input>
+          <input type="button" onClick={()=>AddOrder()} value="send"></input>
+          {Error}
+        </div>
+        {output.input!==""&&<div><span>Input: </span>{output.input}<span> Output: </span>{output.dish}</div>}
+      </div>
+      <table className="tableAddOrder">
+        {HistoryList.map(d=>{
+          return <tr>
+                      <td><span>Input: </span>{d.input}</td>
+                      <td><span>Output: </span>{d.dish}</td>
+                  </tr>             
+        })}
+       </table>
     </div>
   );
 }
